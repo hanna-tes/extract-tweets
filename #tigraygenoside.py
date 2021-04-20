f449b48fe219fdcb13cfced95500e6e2978d02ca
hanna <82633682+hanna-tes@users.noreply.github.com>
2021-04-20 13:15:26 +0300

    Add files via upload

---
+++ #tigraygenoside.py
@@ -0,0 +1,61 @@
+import tweepy
+from tweepy import OAuthHandler
+import pandas as pd
+access_token = '1382759977991933961-LGbCsNFjKoJvyZWV2ntBgqIkdG6fi0'
+access_token_secret = 'AHFDbryqjhqY2JNcUPczovNdBNuwq2KGlUSsnTNQBDpKz'
+consumer_key = 'ZZ5QY6dAWimyDvwOP8bHENXb2'
+consumer_secret = '0bQeoIeEawFUYlAZ4ckOjzBTGDOgHQKIBk3Csl2TLsYnSyWbUW'
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token, access_token_secret)
+api = tweepy.API(auth,wait_on_rate_limit=True)
+# Define the search term and the date_since date as variables
+def scraptweets(search_words, date_since, until_date, numtweets):
+    df_tweets = pd.DataFrame(columns = ['username', 'accdescri', 'location', 'following',
+                                        'followers', 'totaltweets', 'usercreateti', 'tweetcreateti',
+                                        'retweetcount', 'text']
+                                )  
+    tweets = tweepy.Cursor(api.search, q=search_words, lang="en", since=date_since, until=until_date, 
+      	tweet_mode='extended').items(numtweets)
+
+# Store these tweets into a python list
+    tweet_list = [tweet for tweet in tweets]
+    numtweets = 0
+    for tweet in tweet_list:
+# Pull the values
+            username = tweet.user.screen_name
+            accdescri = tweet.user.description
+            location = tweet.user.location
+            following = tweet.user.friends_count
+            followers = tweet.user.followers_count
+            totaltweets = tweet.user.statuses_count
+            usercreateti = tweet.user.created_at
+            tweetcreateti = tweet.created_at
+            retweetcount = tweet.retweet_count
+            try:
+                text = tweet.retweeted_status.full_text
+            except AttributeError:  
+                text = tweet.full_text
+                ith_tweet = [username, accdescri, location, following, followers, totaltweets,
+                         usercreateti, tweetcreateti, retweetcount, text]
+        # Append to dataframe - df_tweets
+
+                df_tweets.loc[len(df_tweets)] = ith_tweet
+         # increase counter
+                numtweets += 1
+    filename = 'scraped_tweets.csv'
+      
+           # save our data as a CSV file.
+    df_tweets.to_csv(filename)
+        
+# Initialise these variables:
+search_words = '#TigrayGenocide'
+date_since = '2021-04-13'
+until_date = '2021-04-15'
+numtweets = 100
+# Call the function scraptweets
+scraptweets(search_words, date_since, until_date, numtweets)
+
+
+
+
+
